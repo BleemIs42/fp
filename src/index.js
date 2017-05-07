451c8fa27207a5408b79c1a18d06b54fd89af0e3
Shafley <shafleyxia@gmail.com>
2017-05-07 21:55:22 +0800

    refactor compose

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-const toFn= x => {
-    if (typeof x === 'function') return x
-    return () => x
-}
-const slice = (arr, ...args) => [].slice.apply(arr, args)
+const toFn= x => (typeof x === 'function') ? x : () => x
 
 let FP = {}
 FP.curry = fn => {
@@ -17,34 +13,36 @@ FP.curry = fn => {
 
 const add = (a, b) => a + b
 const curryAdd = FP.curry(add)
-console.log(curryAdd(2)(3)) // 5
-console.log(curryAdd(1, 2)) // 3
+console.log(curryAdd(4)(2)) // 6
+console.log(curryAdd(2, 4)) // 6
 
-FP.compose = (...fns) => (...args) => {
-    fns = fns.map(x => toFn(x))
-    while (fns.length) {
-        args = [fns.pop().apply(this, args)]
-    }
-    return args[0]
-}
+FP.compose = (...fns) => (...args) => fns.map(x => toFn(x)).reverse().reduce((args, fn) => [fn(...args)], args)[0]
+// FP.compose = (...fns) => (...args) => {
+    // fns = fns.map(x => toFn(x))
+    // while (fns.length) {
+    //     args = [fns.pop().apply(this, args)]
+    // }
+    // return args[0]
+// }
 
-const inc = n => n + 1
+const inc = n => n + 2
 const dob = n => n * 2
 const co = FP.compose(inc, dob)
-console.log(co(2))  // 5
+console.log(co(2))  // 6
 
-FP.composeL = (...args) => FP.compose.apply(this, args.reverse())
+FP.composeL = (...args) => FP.compose(...args.reverse())
 
 const coL = FP.composeL(inc, dob)
-console.log(coL(2)) // 6
+console.log(coL(1)) // 6
 
-FP.pipe = (...args) => (...fns) => {
-    fns = fns.map(x => toFn(x))
-    while (fns.length) {
-        args = [fns.shift().apply(this, args)]
-    }
-    return args[0]
-}
+FP.pipe = (...args) => (...fns) => fns.map(x => toFn(x)).reduce((args, fn) => [fn(...args)], args)[0]
+// FP.pipe = (...args) => (...fns) => {
+//     fns = fns.map(x => toFn(x))
+//     while (fns.length) {
+//         args = [fns.shift().apply(this, args)]
+//     }
+//     return args[0]
+// }
 
-const pp = FP.pipe(2)(inc, dob)
+const pp = FP.pipe(1)(inc, dob)
 console.log(pp)     // 6
