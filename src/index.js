3ab5e27e7fe0f8f2d01b9fb701bd1881fe4f7a2d
Shafley <shafleyxia@gmail.com>
2017-05-08 11:41:52 +0800

    refactor compose

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ let FP = {}
 FP.curry = fn => {
     const f = _args => (...args) => {
         args = [...args, ..._args]
-        if (args.length >= fn.length)
-            return fn(...args)
+        if (args.length >= fn.length) return fn(...args)
         return f(args)
     }
     return f([])
@@ -16,9 +15,9 @@ const curryAdd = FP.curry(add)
 console.log(curryAdd(4)(2)) // 6
 console.log(curryAdd(2, 4)) // 6
 
-FP.compose = (...fns) => (...args) => fns.map(x => toFn(x)).reverse().reduce((args, fn) => [fn(...args)], args)[0]
+FP.compose = (...fns) => fns.reverse().reduce((fn1, fn2) => (...args) => fn2(fn1(...args)))
+// FP.compose = (...fns) => (...args) => fns.reverse().reduce((args, fn) => [fn(...args)], args)[0]
 // FP.compose = (...fns) => (...args) => {
-// fns = fns.map(x => toFn(x))
 // while (fns.length) {
 //     args = [fns.pop().apply(this, args)]
 // }
@@ -35,9 +34,9 @@ FP.composeL = (...args) => FP.compose(...args.reverse())
 const coL = FP.composeL(inc, dob)
 console.log(coL(1)) // 6
 
-FP.pipe = (...args) => (...fns) => fns.map(x => toFn(x)).reduce((args, fn) => [fn(...args)], args)[0]
+FP.pipe = (...args) => (...fns) => fns.reduce((fn1, fn2) => fn2(fn1(...args)))
+// FP.pipe = (...args) => (...fns) => fns.reduce((args, fn) => [fn(...args)], args)[0]
 // FP.pipe = (...args) => (...fns) => {
-//     fns = fns.map(x => toFn(x))
 //     while (fns.length) {
 //         args = [fns.shift().apply(this, args)]
 //     }
