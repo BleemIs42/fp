896e1060993745b22bed0969ac0bc3a76d1d72a8
Shafley <shafleyxia@gmail.com>
2017-05-10 17:14:14 +0800

    publish

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,32 +1,36 @@
-let FP = {}
 
 const curry = (fn, arity = fn.length, ...args0) => (...args) => (_args => _args.length >= arity ? fn(..._args) : curry(fn, arity, ..._args))([...args0, ...args])
-FP.curry = curry
-// FP.curry = (fn, arity = fn.length, ...args0) => {
+// const curry = (fn, arity = fn.length, ...args0) => {
 //     const f = _args => (...args) => ([...args, ..._args].length >= arity ? fn(...args, ..._args) : f([...args, ..._args]))
 //     return f([...args0])
 // }
 
-FP.uncurry = (cfn, ...args0) => (...args) => [...args0, ...args].reduce((fn, arg) => fn(arg), cfn)
+const uncurry = (cfn, ...args0) => (...args) => [...args0, ...args].reduce((fn, arg) => fn(arg), cfn)
 
-FP.compose = (...fns) => fns.reverse().reduce((fn1, fn2) => (...args) => fn2(fn1(...args)))
-// FP.compose = (...fns) => (...args) => fns.reverse().reduce((args, fn) => [fn(...args)], args)[0]
-// FP.compose = (...fns) => (...args) => {
+const compose = (...fns) => fns.reverse().reduce((fn1, fn2) => (...args) => fn2(fn1(...args)))
+// const compose = (...fns) => (...args) => fns.reverse().reduce((args, fn) => [fn(...args)], args)[0]
+// const compose = (...fns) => (...args) => {
 // while (fns.length) {
 //     args = [fns.pop().apply(this, args)]
 // }
 // return args[0]
 // }
 
-FP.composeL = (...args) => FP.compose(...args.reverse())
+const composeL = (...args) => compose(...args.reverse())
 
-FP.pipe = (...args) => (...fns) => fns.reduce((fn1, fn2) => fn2(fn1(...args)))
-// FP.pipe = (...args) => (...fns) => fns.reduce((args, fn) => [fn(...args)], args)[0]
-// FP.pipe = (...args) => (...fns) => {
+const pipe = (...args) => (...fns) => fns.reduce((fn1, fn2) => fn2(fn1(...args)))
+// const pipe = (...args) => (...fns) => fns.reduce((args, fn) => [fn(...args)], args)[0]
+// const pipe = (...args) => (...fns) => {
 //     while (fns.length) {
 //         args = [fns.shift().apply(this, args)]
 //     }
 //     return args[0]
 // }
 
-export default FP
+export default {
+    curry,
+    uncurry,
+    compose,
+    composeL,
+    pipe
+}
